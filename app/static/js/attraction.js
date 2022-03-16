b69ffc32622bfb96467b152944bbde1f231b7b7c
nancy <nancy65018@gmail.com>
2022-03-16 21:00:09 +0800

    change JS to MVC structor

--- app/static/js/attraction.js
+++ app/static/js/attraction.js
@@ -3,56 +3,67 @@ const imageContainer = document.querySelector('.img-container');
 const spotContainer = document.querySelector('.spot-container');
 const titleContainer = document.querySelector('.profile>.title');
 const subtitleContainer = document.querySelector('.profile>.subtitle');
-const descContainer = document.createElement('div');
-const addressContainer = document.createElement('div');
-const transportContainer = document.createElement('div');
-const addressTitle = document.createElement('div');
-const transportTitle = document.createElement('div');
-const addressContent = document.createElement('div');
-const transportContent = document.createElement('div');
 const innerImgContainer = document.querySelector('.inner-img-container');
 const [path, id] = window.location.pathname.split('/attraction/');
+let data;
 
-fetch(`/api/attraction/${id}`)
-.then((response) => {
-    if(response.ok){
-        return response.json()
-    }
-}).then((data) => {
-    init(data['data']);
-}).catch((e) => {
-    console.log(e);
-})
+//Start point
+fetchAPI(id)
 
-function init(data){
-    const title = data['name'];
-    const subtitle = data['category'] + ' at ' + data['mrt'];
-    const desc = data['description'];
-    const address = data['address'];
-    const transport = data['transport'];
-    const images = data['images'];
+//Fetch backend API
+function fetchAPI(id){
+    fetch(`/api/attraction/${id}`)
+    .then((response) => {
+        if(response.ok){
+            return response.json()
+        }
+    }).then((jsonData) => {
+        data = jsonData['data'];
+        init()
+    }).catch((e) => {
+        console.log(e);
+    })
+}
 
+//Create empty element
+function createElement(){
+    const descContainer = document.createElement('div');
+    const addressContainer = document.createElement('div');
+    const transportContainer = document.createElement('div');
+    const addressTitle = document.createElement('div');
+    const transportTitle = document.createElement('div');
+    const addressContent = document.createElement('div');
+    const transportContent = document.createElement('div');
+
+    return {"descContainer":descContainer, "addressContainer":addressContainer, "transportContainer":transportContainer, "addressTitle":addressTitle, "transportTitle":transportTitle, "addressContent":addressContent, "transportContent":transportContent}
+}
+
+//Append data in empty element
+function appendElement(element, title, subtitle, desc, address, transport){
     titleContainer.textContent = title;
     subtitleContainer.textContent = subtitle;
-    descContainer.textContent = desc;
-    descContainer.setAttribute('class', 'desc-container');
 
-    addressTitle.textContent = '景點地址：';
-    addressContent.textContent = address;
-    addressContainer.appendChild(addressTitle);
-    addressContainer.appendChild(addressContent);
-    addressContainer.setAttribute('class', 'address-container');
+    element["descContainer"].textContent = desc;
+    element["descContainer"].setAttribute('class', 'desc-container');
 
-    transportTitle.textContent = '交通方式：';
-    transportContent.textContent = transport;
-    transportContainer.appendChild(transportTitle);
-    transportContainer.appendChild(transportContent);
-    transportContainer.setAttribute('class', 'transport-container');
+    element["addressTitle"].textContent = '景點地址：';
+    element["addressContent"].textContent = address;
+    element["addressContainer"].appendChild(element["addressTitle"]);
+    element["addressContainer"].appendChild(element["addressContent"]);
+    element["addressContainer"].setAttribute('class', 'address-container');
 
-    main.appendChild(descContainer);
-    main.appendChild(addressContainer);
-    main.appendChild(transportContainer);
+    element["transportTitle"].textContent = '交通方式：';
+    element["transportContent"].textContent = transport;
+    element["transportContainer"].appendChild(element["transportTitle"]);
+    element["transportContainer"].appendChild(element["transportContent"]);
+    element["transportContainer"].setAttribute('class', 'transport-container');
 
+    return {"descContainer":element["descContainer"], "addressContainer":element["addressContainer"], "transportContainer":element["transportContainer"]}
+}
+
+//separate images process
+function initImage(){
+    const images = data['images'];
     for(let i = 0; i < images.length; i++){
         const img = document.createElement('img');
         const spot = document.createElement('div');
@@ -80,68 +91,110 @@ function init(data){
     }
 }
 
+function init(){
+    const element = createElement();
+
+    const title = data['name'];
+    const subtitle = data['category'] + ' at ' + data['mrt'];
+    const desc = data['description'];
+    const address = data['address'];
+    const transport = data['transport'];
+
+    const container = appendElement(element, title, subtitle, desc, address, transport);
+
+    main.appendChild(container["descContainer"]);
+    main.appendChild(container["addressContainer"]);
+    main.appendChild(container["transportContainer"]);
+
+    initImage();
+}
+
+
 //carousel controller
 const leftBtn = document.querySelector('.arrow-container').children[0];
 const rightBtn = document.querySelector('.arrow-container').children[1];
+
+function changeSpot(showSpot, removeSpot){
+    showSpot.setAttribute('class', 'inner-spot');
+    removeSpot.removeAttribute('class', 'inner-spot');
+}
+
+function prevImage(showImg, rightImg, totalPage){
+    let index ,leftImg, showSpot, removeSpot;
+
+    index = parseInt(rightImg.id)-1;
+    if(index < 0) {index = totalPage - 1}
+
+    leftImg = document.getElementById(`${index}`);
+
+    showImg.setAttribute('class', 'left');
+    leftImg.setAttribute('class', 'right');
+    rightImg.setAttribute('class', 'show');
+
+    showSpot = document.getElementById(`spot_${rightImg.id}`);
+    removeSpot = document.getElementById(`spot_${showImg.id}`);
+    changeSpot(showSpot, removeSpot)
+}
+
+function nextImage(showImg, rightImg, totalPage){
+    let index ,leftImg, showSpot, removeSpot;
+
+    index = parseInt(showImg.id)+1;
+    if(index >= totalPage) index = 0;
+
+    leftImg = document.getElementById(`${index}`);
+
+    if(totalPage === 2){
+        showImg.setAttribute('class', 'right');
+        leftImg.setAttribute('class', 'show');
+    }else{
+        showImg.setAttribute('class', 'right');
+        leftImg.setAttribute('class', 'show');
+        rightImg.setAttribute('class', 'left');
+    }
+
+    showSpot = document.getElementById(`spot_${index}`);
+    removeSpot = document.getElementById(`spot_${showImg.id}`);
+    changeSpot(showSpot, removeSpot)
+}
 
 function changeImage(e){
     const target = e.target.id;
     const showImg = document.querySelector('.show');
     const rightImg = document.querySelector('.right');
     const totalPage = document.querySelector('.spot-container').children.length;
-    let index ,leftImg, showSpot, removtSpot;
 
     if(target === 'left'){
-        index = parseInt(rightImg.id)-1;
-        if(index < 0) {index = totalPage - 1}
-
-        leftImg = document.getElementById(`${index}`);
-        showSpot = document.getElementById(`spot_${rightImg.id}`);
-        removtSpot = document.getElementById(`spot_${showImg.id}`);
-
-        showImg.setAttribute('class', 'left');
-        leftImg.setAttribute('class', 'right');
-        rightImg.setAttribute('class', 'show');
-        
+        prevImage(showImg, rightImg, totalPage)
     }else if(target === 'right'){
-        index = parseInt(showImg.id)+1;
-        if(index >= totalPage) index = 0;
-
-        leftImg = document.getElementById(`${index}`);
-        showSpot = document.getElementById(`spot_${index}`);
-        removtSpot = document.getElementById(`spot_${showImg.id}`);
-
-        showImg.setAttribute('class', 'right');
-        leftImg.setAttribute('class', 'show');
-        rightImg.setAttribute('class', 'left');
+        nextImage(showImg, rightImg, totalPage)
     }
-    showSpot.setAttribute('class', 'inner-spot');
-    removtSpot.removeAttribute('class', 'inner-spot');
 }
-
 rightBtn.addEventListener('click', changeImage);
 leftBtn.addEventListener('click', changeImage);
+
 
 //ticket controller
 const amBtn = document.querySelector('#am');
 const pmBtn = document.querySelector('#pm');
 
+function changeAttribute(click, unClick, ticketPrice){
+    const price = document.querySelector('#price');
+    click.setAttribute('class', 'inner-radio-btn');
+    unClick.removeAttribute('class', 'inner-radio-btn');
+    price.textContent = `新台幣 ${ticketPrice} 元`;
+}
+
 function changePrice(e){
     const target = e.target.id;
     const amInnerBtn = document.querySelector('#am>div');
     const pmInnerBtn = document.querySelector('#pm>div');
-    const price = document.querySelector('#price');
     
     if(target === 'am'){
-        amInnerBtn.setAttribute('class', 'inner-radio-btn');
-        pmInnerBtn.removeAttribute('class', 'inner-radio-btn');
-        price.textContent = '新台幣 2000 元';
+        changeAttribute(amInnerBtn, pmInnerBtn, price=2000)
     }else if(target === 'pm'){
-        pmInnerBtn.setAttribute('class', 'inner-radio-btn');
-        amInnerBtn.removeAttribute('class', 'inner-radio-btn');
-        price.textContent = '新台幣 2500 元';
+        changeAttribute(pmInnerBtn, amInnerBtn, price=2500)
     }
 }
-
 amBtn.addEventListener('click', changePrice);
 pmBtn.addEventListener('click', changePrice);
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,10 +1,15 @@
 const main = document.querySelector('main');
 const errorDiv = document.createElement('div');
+const searchForm = document.querySelector('.searchForm');
+const word = document.querySelector('input');
 const initpage = 0;
 const initKeyword = '';
+let data;
 
+//Start point
 fetchApi(initpage, initKeyword);
 
+//Fetch backend API
 function fetchApi(page, keyword){
     fetch(`/api/attractions?page=${page}&keyword=${keyword}`,{
         method: "GET"
@@ -13,8 +18,9 @@ function fetchApi(page, keyword){
         if(response.ok){
             return response.json()
         }
-    }).then((data) => {
-        init(data['data'], data['nextPage'], keyword)
+    }).then((jsonData) => {
+        data = jsonData['data'];
+        init(jsonData['nextPage'], keyword)
     }).catch((e) => {
         errorDiv.textContent = `查無 '${keyword}' 的結果`;
         errorDiv.setAttribute('class', 'errorDiv');
@@ -22,51 +28,53 @@ function fetchApi(page, keyword){
     })
 }
 
-function init(data, page, keyword){
-    for(let i = 0; i < data.length; i++){
-        const divContainer = document.createElement('div');
-        const imgContainer = document.createElement('div');
-        const img = document.createElement('img');
-        const info = document.createElement('div');
-        const title = document.createElement('div');
-        const subtitle = document.createElement('div');
-        const subtitleMrt = document.createElement('div');
-        const subtitleCategory = document.createElement('div');
-        const link = document.createElement('a');
+//Create empty element
+function createElement(){
+    const divContainer = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const info = document.createElement('div');
+    const title = document.createElement('div');
+    const subtitle = document.createElement('div');
+    const subtitleMrt = document.createElement('div');
+    const subtitleCategory = document.createElement('div');
+    const link = document.createElement('a');
 
-        const name = data[i]['name'];
-        const mrt = data[i]['mrt'];
-        const category = data[i]['category'];
-        const image = data[i]['images'][0];
-        const id = data[i]['id'];
-        
-        img.src = image;
-        title.textContent = name;
-        subtitleMrt.textContent = mrt;
-        subtitleCategory.textContent = category;
-        link.href = `/attraction/${id}`;
+    return {"divContainer":divContainer,"imgContainer":imgContainer,"img":img,"info":info,"title":title,"subtitle":subtitle,"subtitleMrt":subtitleMrt,"subtitleCategory":subtitleCategory,"link":link}
+}
 
-        imgContainer.appendChild(img);
-        subtitle.appendChild(subtitleMrt);
-        subtitle.appendChild(subtitleCategory);
-        title.setAttribute('class', 'title');
-        subtitle.setAttribute('class', 'subtitle');
-        imgContainer.setAttribute('class', 'imageContainer');
+//Append data in empty element
+function appendElement(element, name, mrt, category, image, id, index){
+    element["img"].src = image;
+    element["title"].textContent = name;
+    element["subtitleMrt"].textContent = mrt;
+    element["subtitleCategory"].textContent = category;
+    element["link"].href = `/attraction/${id}`;
+    
+    element["imgContainer"].appendChild(element["img"]);
+    element["subtitle"].appendChild(element["subtitleMrt"]);
+    element["subtitle"].appendChild(element["subtitleCategory"]);
+    element["title"].setAttribute('class', 'title');
+    element["subtitle"].setAttribute('class', 'subtitle');
+    element["imgContainer"].setAttribute('class', 'imageContainer');
+    
+    element["info"].appendChild(element["title"]);
+    element["info"].appendChild(element["subtitle"]);
+    element["info"].setAttribute('class', 'info');
 
-        info.appendChild(title);
-        info.appendChild(subtitle);
-        info.setAttribute('class', 'info');
+    element["link"].appendChild(element["imgContainer"]);
+    element["link"].appendChild(element["info"])
+    element["divContainer"].appendChild(element["link"]);
 
-        link.appendChild(imgContainer);
-        link.appendChild(info)
-        divContainer.appendChild(link);
-        if(i === data.length-1){
-            divContainer.setAttribute('class', 'last-child')
-        }
-
-        main.appendChild(divContainer);
+    if(index === data.length-1){
+        element["divContainer"].setAttribute('class', 'last-child');
     }
     
+    return element["divContainer"]
+}
+
+//Loading observator
+function observator(page, keyword){
     const lastOne = document.querySelector('.last-child');
     if(lastOne){
         const observer = new IntersectionObserver(entries => {
@@ -75,7 +83,7 @@ function init(data, page, keyword){
             if(page == 'NULL'){
                 observer.unobserve(lastOne);
             }else{
-                fetchApi(page, keyword)
+                fetchApi(page, keyword);
                 observer.unobserve(lastOne);
                 lastOne.removeAttribute('class', 'last-child');
             }
@@ -86,17 +94,34 @@ function init(data, page, keyword){
     }
 }
 
-//search
-const searchForm = document.querySelector('.searchForm');
-const word = document.querySelector('input');
+function init(page, keyword){
+    for(let i = 0; i < data.length; i++){
+        const element = createElement();
 
-function search(e){
+        const name = data[i]['name'];
+        const mrt = data[i]['mrt'];
+        const category = data[i]['category'];
+        const image = data[i]['images'][0];
+        const id = data[i]['id'];
+        
+        const divContainer = appendElement(element, name, mrt, category, image, id, index=i)
+        
+        main.appendChild(divContainer);
+    }
+    observator(page, keyword);
+}
+
+//Remove init data
+function removeData(){
     const divs = document.querySelectorAll('main > div');
-
-    e.preventDefault();
     divs.forEach((div) => {
         div.remove();
     })
+}
+//search
+function search(e){
+    e.preventDefault();
+    removeData()
     fetchApi(initpage, word.value);
     word.value = '';
 }
