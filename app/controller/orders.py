4e0d1314ebc2aaab9f099335e66952c7f3812c66
nancy <nancy65018@gmail.com>
2022-04-07 19:38:37 +0800

    check db again

--- app/controller/orders.py
+++ app/controller/orders.py
@@ -52,14 +52,17 @@ def create_order():
                         "message": response_content["msg"]
                     }
                 }
-                order_status = get_db("INSERT INTO orders (orderId, paymentStatus, orderName, orderEmail, orderPhone, orderItem, orderUser, attraction, address, image)VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", [order_number,0, contact['name'], contact['email'], contact['phone'], attraction['id'], email, attraction['name'], attraction['address'], attraction['image']], 'none')
+                print(type(attraction['image']))
+                order_status = get_db("INSERT INTO orders (orderId, paymentStatus, orderName, orderEmail, orderPhone, orderItem, orderUser)VALUES(%s, %s, %s, %s, %s, %s, %s)", [order_number,0, contact['name'], contact['email'], contact['phone'], attraction['id'], email], 'none')
+                test = get_db("UPDATE orders SET attraction=%s, address=%s, date=%s, time=%s ,price=%s WHERE orderId=%s", [attraction['name'], attraction['address'], booking['date'], booking['time'], order['price'], order_number], 'none')
+                testimg = get_db("UPDATE orders SET image=%s WHERE orderId=%s", [attraction['image'], order_number], 'none')
                 # order_status = get_db("INSERT INTO orders (orderId, paymentStatus, orderName, orderEmail, orderPhone, orderItem, orderUser, date, time, price, attraction, address, image)VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", [order_number, 0, contact['name'], contact['email'], contact['phone'], attraction['id'], email, booking['date'], booking['time'],order['price'], attraction['name'], attraction['address'], attraction['image']], 'none')
                 # update_status = get_db("UPDATE booking SET paymentStatus=0 WHERE bookingItem=%s", [attraction['id']], 'none')
                 # undelete_items = get_db("SELECT bookingItem FROM booking WHERE bookingUser=%s AND paymentStatus=1", [email], 'all')
                 # if(undelete_items != []):
                 #     for item in undelete_items:
                 #         delete_item = get_db("DELETE FROM booking WHERE bookingItem=%s", [item['bookingItem']], 'none')
-                response = make_response({"order": order_status}, 200)
+                response = make_response({"order": order_status, "test": test, "testimg": testimg}, 200)
                 # response = make_response({"data": order_status}, 200)
             else:
                 response = make_response({"error": True, "message": "Failed to pay", "order_number": order_number}, 400)
